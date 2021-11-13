b17ed7ba103634452855635f405381049f7490b2
Aristidis13 <aris13pat@hotmail.com>
2021-11-13 23:50:10 +0200

    Fixed issue of React Loading alongside Babel. React now works in site.

--- js/react.js
+++ js/react.js
@@ -1,28 +1,28 @@
+console.log('I read this file');
+
 class ProjectList extends React.Component {
     render() { 
         return (
             <section className="project-info">
-                <h3 className="project-title"> My Projects</h3>
-                &lt;&excl;-- This is the block that contains the presentational note --&gt;
-                <p class="project-description">
+                <h3 className="project-title"> My Most Interesting Projects</h3>
+                <p class="project-intro">
                     This is a collection of all the projects I wanted to make publicly available. Some of them are on Github while others exist only here.
                     Also in some here can be found the exercise and in Github can be found the full implementation.
                     All projects are under the MIT License as described in Github.
                     In a nutshel, that means that you can use whatever you find useful but you cannot claim it as yours.
                 </p>
+                <Project />
             </section>
         );
     }
 }
-ReactDOM.render(
-<ProjectList />, document.getElementById('projects')
-);
+
 
 class Project extends React.Component {
     render() {
         return (
             <section className="project-info">
-                <h3 className="project-title"> My Projects</h3>
+                <h3 className="project-title"> Bash Scripting and Semaphores with C</h3>
                 <p className="project-description">
                     This is a compilation of a plethora of subjects. Fork-join programs, semaphores implemented in C, Bash Scripting and theoritical analysis
                     for a few Scheduling Algorithms (ex. Round-Robbin) are the most important elements of this project. Code can be found in Github
@@ -33,6 +33,7 @@ class Project extends React.Component {
         );
     }
 }
+
 ReactDOM.render(
-<ProjectList />, document.getElementById('projects')
+    <ProjectList />, document.getElementById('projects')
 );
