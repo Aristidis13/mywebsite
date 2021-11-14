513f6206f0c0aa5a756ed8b4d893a0745a298156
Aristidis13 <aris13pat@hotmail.com>
2021-11-14 02:56:49 +0200

    Added the final version of projects.js file that contains the object

---
+++ projects.js
@@ -0,0 +1,37 @@
+const projects = [
+	{
+		"title": "Parser and Lexicological Analyser with Flex & Bison",
+		"description": "A parser that accepts as input a JSON file that contains multiple tweets.retweets and extended tweets and finds what fields are missing or have different values from what is expected. The JSON file can be a complex one in the sense that one field can contain JSON arrays. The project was created by C although Flex and Bison files can also run with C++.Full implementation is in Github .",
+		"link": "https://github.com/Aristidis13/Flex-and-Bison-Parser"
+	},
+	{
+		"title": "EstateHub",
+		"description": "That was a lengthy project. The code can be found in Github. It's about the development of a business idea and it has been implemented in C# and wpf as a windows binary. ICONIX was the software development methodology that was followed and the files have the final use cases,robustness diagrams and sequence diagrams among other files, for this project.",
+		"link": "https://github.com/katagis/estatehub"
+	},
+	{
+		"title": "Social Media Network",
+		"description": "This is a social Network that was created for the Java course in my college. It is a solid long backend project written in Java. Project is created in purely OOP fashion and contains a lot of methods for diffrent things such as posting,likinig a post, commenting,messaging,friend requests and more.",
+		"link" :"https://github.com/Aristidis13/Social-Network"
+	},
+	{
+		"title": "Parallel Programming with OpenMP Framework",
+		"description": "The project examines 4 schedules of parallelization and their speed perforfance for a non-distributed workload with the OpenMP Framework and C++ as the programming language. I also optimized the code for speed and performance. Operating System that was used is UBUNTU 18.04.02 edition.",
+		"link" :"https://github.com/Aristidis13/Parallellization-with-OpenMP-Framework/"
+	},
+	{
+		"title": "Bash Scripting (& awk) in a logfile",
+		"description": "A Bash Script that performs a multitude of calculations in a log file using awk and sed and presents the results",
+		"link": "https://github.com/Aristidis13/Bash_Scripting"
+	},
+	{
+		"title": "A Crowdsourcing Project",
+		"description": "A web developement project that is a deep dive in the world of web development. It contains ajax handling,form validation in the frontend and backend,styling, and semantic HTML. It is my first truly lengthy project and it is about the creation of a crowdsourcing project that gives the opportunity to the user to upload files with his google location history data, see in charts the results of his uploads. select and see in a heatmat his movements, estimate by the sensors of his smartphone how much he uses his body (ecological score) for his movements. He can signup as a user by providing his email,password and his username of choice. The system use 2-way encryption techniques and hashing to ensure security for the database. Also the second type of user is the admin who can see all the results from the uploads of the users, can select and see in a heatmap the results of uploaded data for a specific period of time and can export the data as a CSV,XML or a JSON file. Both users must login and be validated from the database to access their main page.",
+		"link": "https://github.com/Aristidis13/Crowdsourcing-project"
+	},
+	{
+		"title": " Database Project in MySQL",
+		"description": "This is a project that is about relational MySQL Database handling. A Database about an erecruitment system is given, and you have to add more tables and appropriate information to them. Also queries have to be constructed that extract information from the tables. Specifically, there are two types of users that interact with the database.A recruiter and a candidate.It contains create statements, insertions, select statements,triggers and stored proecedures",
+		"link": "https://github.com/Aristidis13/Crowdsourcing-project"
+	}
+];
