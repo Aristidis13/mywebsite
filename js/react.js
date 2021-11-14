fcf17d5271f8fdfa0d01fb097a35a240fef2efb9
Aristidis13 <aris13pat@hotmail.com>
2021-11-14 16:40:31 +0200

    Fixed issue of dynamic rendering alongside semantic HTML

--- js/react.js
+++ js/react.js
@@ -8,16 +8,16 @@ class ProjectList extends React.Component {
             />
         ));
         return (
-            <section className="project-info">
-                <h3 className="project-title"> My Most Interesting Projects</h3>
-                <p class="project-intro">
-                    This is a collection of all the projects I wanted to make publicly available  and consider the nicest.
-                    You should view my whole work in Github.
-                    If you have any interesting idea that you think can help us both improve or you are searching for collaborations
-                    with other programmers or think that you want to say hello contact me.
-                </p>
-                {projectComponents}
-            </section>
+            <article id="projects">
+            <h2 className="project-title"> My Most Interesting Projects</h2>
+            <p class="project-intro">
+                This is a collection of all the projects I wanted to make publicly available  and consider the nicest.
+                You should view my whole work in Github.
+                If you have any interesting idea that you think can help us both improve or you are searching for collaborations
+                with other programmers or think that you want to say hello contact me.
+            </p>
+            {projectComponents}
+        </article>
         );
     }
 }
@@ -26,18 +26,20 @@ class ProjectList extends React.Component {
 class Project extends React.Component {
     render() {
         return (
-            <a href={this.props.link} target="_blank">
             <section className="project-info">
-                <h3 className="project-title"> {this.props.title}</h3>
-                <p className="project-description">
-                    {this.props.description}
-                </p>
+                <a href={this.props.link} target="_blank">
+                    <section class="project-info">
+                        <h3 class="project-title"> {this.props.title}</h3>
+                        <p class="project-description">
+                            {this.props.description}
+                        </p>
+                    </section>
+                </a>
             </section>
-            </a>
         );
     }
 }
 
 ReactDOM.render(
-    <ProjectList />, document.getElementById('projects')
+    <ProjectList />, document.getElementById('projects-section')
 );
