94e495830690bc004ebea917b3300a7afced58b3
Aris Barlos <aristidis.barlos@gmail.com>
2024-12-28 01:33:16 +0200

    Fixes and adds X to the welcome animation

--- js/react.js
+++ js/react.js
@@ -5,15 +5,34 @@ class App extends React.PureComponent {
       welcomeSectionIsVisible: true,
     };
     this.hide = this.hide.bind(this);
+    this.clickBackToTop = this.clickBackToTop.bind(this);
   }
   hide() {
     this.setState({ welcomeSectionIsVisible: false });
     document.documentElement.style.overflowY = "scroll"
   }
+
+  clickBackToTop() {
+    if(this.state.welcomeSectionIsVisible) {
+      // Add the animation class
+      document.getElementById('welcome-view').classList += "divAnimation";
+      
+      // Wait the animation before clearing it
+      setTimeout(()=> {
+        this.hide();
+      },1500);
+    } else if(!this.state.welcomeSectionIsVisible) {
+      window.scrollTo(0,0);
+    }
+  };
+
   render() {
     return <>
       {this.state.welcomeSectionIsVisible && <WelcomeView hide={this.hide} />}
       <h1 style={{ display: "none" }}> Aris Barlos a Frontend Developer!</h1>
+      <button onClick={this.clickBackToTop} id="back-to-top">
+        <img src={this.state.welcomeSectionIsVisible ? "/photos/x.svg" : "/photos/up.svg"} />
+      </button>
       <ProjectList />
       <ExperienceList />
       <SkillList />
